fd2f592b383fee2e266e4c41f24375609672a1c9
hemel17 <redewanmahmud@gmail.com>
2025-03-10 16:42:37 +0600

    user schema started

---
+++ models/User.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+      minLength: [8, "Password must be at least 8 characters long."],
+      maxLength: [32, "Password cannot be more than 32 characters."],
+      select: false,
+    },
+    phone: {
+      type: String,
+      trim: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    accountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: Number,
+    verificationCodeExpire: Date,
+    resetPasswordToken: String,
+    resetPasswordTokenExpire: Date,
+  },
+  { timestamps: true }
+);
+
+// * hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// * check if the password is correct
